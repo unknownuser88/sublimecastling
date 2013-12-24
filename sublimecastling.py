01f5303c1d12046c3590741deab89220e35943c8
unknownuser88 <davidbekoyan@gmail.com>
2013-12-23 23:36:25 -0800

    Update sublimecastling.py

--- sublimecastling.py
+++ sublimecastling.py
@@ -5,9 +5,12 @@ class SublimecastlingCommand(sublime_plugin.TextCommand):
 		view = self.view;
 		try:
 			view.sel()[1];
-			s1 = view.substr(view.sel()[0]);
-			s2 = view.substr(view.sel()[1]);
-			self.view.replace(edit, view.sel()[0], s2);
-			self.view.replace(edit, view.sel()[1], s1);
+			if not (view.sel()[0].empty() and view.sel()[1].empty()
+				s1 = view.substr(view.sel()[0]);
+				s2 = view.substr(view.sel()[1]);
+				self.view.replace(edit, view.sel()[0], s2);
+				self.view.replace(edit, view.sel()[1], s1);
+			else:
+				sublime.status_message('Please Make A Selection');
 		except IndexError:
 			sublime.status_message('Please Make A Selection');
